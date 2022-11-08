0e005ca22595446f4b7d3353278c40a26c3a3d03
Alvin Tan De Jun <>
2022-11-08 17:51:37 +0800

    update module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .resnet import resnet18, resnet34
 from .customtensorboard import CustomTensorBoard
 from .supconloss import SupervisedContrastiveLoss
+from .CosineAnnealWithWarmup import CosineAnnealWithWamrup
